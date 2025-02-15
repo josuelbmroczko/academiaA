8c6849eee63c791056788fd55fd36c7887b138ee
Josuel Beserra Mroczko <josuelbeserramroczko@gmail.com>
2025-02-15 17:14:24 -0300

    a

--- src/componetes/contatos/index.jsx
+++ src/componetes/contatos/index.jsx
@@ -10,11 +10,16 @@ export default function Contatos() {
            <h2>Contatos</h2>    
         <ul>
               
-              <li><a href="tel:+551199999999"><img src={imagem3} alt="telefone" /></a></li>
+              <li><a href="https://wa.me/5511998731174?text=vim%20atrav%C3%A9s%20do%20site" target="blanck"><img src={imagem3} alt="telefone" /></a></li>
           </ul>
             <ul>
               
-                <li><a href="tel:+551199999999"><img src={imagem2} alt="telefone" /></a></li>
+            <li>
+  <a href="tel:+5511998731174">
+    <img src={imagem2} alt="telefone" />
+  </a>
+</li>
+
             </ul>
 
             <ul>
